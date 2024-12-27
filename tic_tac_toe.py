c669451766eb793bb8ff9a6d9557c5f593f85091
cesium132dot9 <sunc20060429@gmail.com>
2024-12-26 18:07:10 -0600

    worked on

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,42 +4,54 @@
 
 
 class TicTacToe: 
-# empty board 
-    board = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
+    """A custom object for the game of Tic Tac Toe."""
+    board: list[list[int]] 
+    current_player: int
+    player1: tuple[str, int]
+    player2: tuple[str, int]
 
-    run_game = True
-    player = 1
-    player1_name = ''
-    player2_name = ''
+    def __init__(self, player1name: str, player2name: str) -> None: 
+        """Initializes the game of Tic Tac Toe."""
+        self.board = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]]
+        self.current_player = 1
+        self.player1 = player1name, 1
+        self.player2 = player2name, 2
 
-    def move(row: int, column: int, player: int, board: list[list[int]]) -> bool: 
-        # actually inputting the move
+    def move(self, board: list[list[int]], row: int, column: int, player: int) -> None: 
+        """Making a move on the board."""
         if player == 1: 
             board[row - 1][column - 1] = 1
-            return True
         elif player == 2: 
             board[row - 1][column - 1] = 2
-            return True
         else: 
             return False
         
-    def check_move(row: int, column: int, board: list[list[int]]) -> bool:
-        if row > 3 or column > 3:  # checking if the move is out of range 
+    def check_move(self, row: int, column: int, board: list[list[int]]) -> bool:
+        """Checking if the move is valid. Done by checking if the move is out or range then if a player has already 
+        played on that location.
+        """
+        # checking if the move is out of range
+        if row > 3 or column > 3:   
             return False
-        elif board[row - 1][column - 1] == 1 or board[row - 1][column - 1] == 2:  # checking if a player has already played on that square
+        # checking if a player has already played on that square
+        elif board[row - 1][column - 1] == 1 or board[row - 1][column - 1] == 2:  
             return False
 
-    def win_condition(board: list[list[int]]) -> bool:
+    def win_condition(self, board: list[list[int]]) -> bool:
+        """Checking if a player has won the game. First checking if there are 3 in a row or column. Then checking the diagonals."""
+        # checking rows and columns 
         for i in range(3):
+            # checking if there are 3 in a row
             if board[i][0] == board[i][1] == board[i][2] == 1 or \
-                board[i][0] == board[i][1] == board[i][2] == 2:  # checking if there are 3 in a row 
+                board[i][0] == board[i][1] == board[i][2] == 2:   
                 return True
+            # checking if there are 3 in a column
             if board[0][i] == board[1][i] == board[2][i] == 1 or \
-                board[0][i] == board[1][i] == board[2][i] == 2:  # checking if there are 3 in a column 
+                board[0][i] == board[1][i] == board[2][i] == 2:  
                 return True 
-
+            
         # checking diagonals
         # checking top left to bottom right 
         if board[0][0] == board[1][1] == board[2][2] == 1 or \
@@ -50,4 +62,5 @@ class TicTacToe:
             board[0][2] == board[1][1] == board[2][0] == 2: 
             return True
         
+        # if no player has won 
         return False
